52f21ed95dde3606cfb2389921d59a67cad380e3
huynhtancuong <huynhtancuong.hcm@gmail.com>
2021-12-18 02:23:12 +0300

    created Lab4 based on Lab3

---
+++ Lab3_my/src/main/java/Test/Test.java
@@ -0,0 +1,44 @@
+package Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class t = Class.forName("NonAnimals.Doors");
+        System.out.println(t.getName());
+        try {
+            chia2so(2, 0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void chia2so(int a, int b) throws Exception {
+        try {
+            System.out.println(a/b);
+        } catch (Exception e) {
+            throw new Exception("Loi chia cho so 0.");
+        }
+    }
+}
+
+
+class MyException extends Exception {
+    private String error;
+
+    public MyException(String error) {
+        super("Fuck");
+        this.error = error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+}
